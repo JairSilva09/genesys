afd963f44a637b392622da16de1e46fe07f215d1
Jair Silva <harryhaller30@gmail.com>
2022-11-22 10:36:22 -0500

    primer commit se han creado componentes side menu menu y dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MenuComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
