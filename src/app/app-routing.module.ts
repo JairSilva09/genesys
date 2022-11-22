37615c422ee5b234b7a439110a57197a89523d0e
Jair Silva <harryhaller30@gmail.com>
2022-11-22 12:09:07 -0500

    add route /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   {path: 'dashboard',component: DashboardComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
 
 @NgModule({
