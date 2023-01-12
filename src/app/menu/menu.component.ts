9a0f3c1513182b5a6f4cd32ab451ce1e69dfeb1a
jair silva <harryhaller30@gmail.com>
2023-01-12 14:24:18 -0500

    add remove function

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  LIST_FALSE: any[] = [
+    {
+      skill : "Clink Agency Ext Res",
+      queue: "2",
+      wait: "00:03:19"
+    },
+    {
+      skill : "Dish Residencial",
+      queue: "1",
+      wait: "00:00:01"
+    },
+    {
+      skill : "Cox",
+      queue: "1",
+      wait: "00:00:05"
+    },
+    {
+      skill : "VopIP",
+      queue: "1",
+      wait: "00:00:44"
+    },
+    {
+      skill : "Verizon Zip LookUp",
+      queue: "1",
+      wait: "00:01:53"
+    }
+  ]
+
+
 }
